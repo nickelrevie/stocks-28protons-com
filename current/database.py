c3fa633962f83db7dc903304e6a31fb08d82955d
Mechanickel <nickel.revie@gmail.com>
2018-12-26 23:27:47 -0800

    minor changes
    Wrote small amount on deskop. Merging so I have access to it on my laptop

---
+++ current/database.py
@@ -0,0 +1,10 @@
+# This class talks to the database on behalf of the program. It's so
+# each class doesn't need its own code to connect to the database.
+class DBInterface:
+
+    # When it's initialized, do nothing
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
--- current/symbolvalidator.py
+++ current/symbolvalidator.py
@@ -6,3 +6,7 @@ class SymbolValidator:
     # When it's initialized, do nothing
     def __init__(self):
         pass
+
+    # Initiates the process to check whether the input symbols exist
+    def checkSymbols(self):
+        pass
